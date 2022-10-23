500c61741ff71c49c9d4d2477ab4ecd11108f50a
Allen <qs96649006@gmail.com>
2022-10-23 19:06:53 +0800

    add function

--- app.py
+++ app.py
@@ -38,10 +38,20 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = "你吃飯了嗎?"
+    r = "很抱歉,你說啥?"
+
+    if msg in ["hi", "Hi"]:
+        r = "嗨"
+    elif msg == "你吃飯了嗎?":
+        r = "你吃屎了嗎?"
+    elif msg == "你是誰":
+        r = "我是你爸, 我在你後面很火"
+    elif "訂位" in msg:
+        r = "你想訂位是嗎?"
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
